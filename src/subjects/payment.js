1c3625a954c6d96a3bc06f811ab7008c2cc216f1
JoaoVitorLima242 <jlima0500@gmail.com>
2024-02-19 14:46:19 -0300

    feat: subject methods

--- src/subjects/payment.js
+++ src/subjects/payment.js
@@ -1,5 +1,13 @@
 export default class PaymentSubject {
-  notify(data) { }
-  unsubscribe(observable) { }
-  subscribe(observable) { }
+  #observers = new Set()
+
+  notify(data) {
+    this.#observers.forEach(observer => observer.update(data))
+  }
+  unsubscribe(observable) {
+    this.#observers.delete(observable)
+  }
+  subscribe(observable) {
+    this.#observers.add(observable)
+  }
 }
